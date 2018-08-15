500aa4c726bc5b199b7726cc81bcadaec5f4f5a7
QaysTrad <qaystrad@gamil.com>
2018-08-15 15:21:54 +0300

    get the data from the server

--- angular-client/components/catsList.js
+++ angular-client/components/catsList.js
@@ -4,27 +4,37 @@ app.component('cats', {
 	templateUrl: '/templates/adoptCat.html'
 });
 app.controller('catsCtrl', function ($scope, $http,$window){
+  $scope.imgHere ='https://pbs.twimg.com/profile_images/963453873812893696/wRf1-zn-_400x400.jpg';
+
 var getCats = function() {
-	// TODO: Your code here
+	  $http({
+    method:'GET',
+    url:'/cats',
+    }).then(function (data){
+      $scope.cats = data.data
+
+    }, function (err) {
+      console.log('error' , err)
+    })
 
 };
 getCats();
 
 $scope.addCats=function() {
-// TODO: Your code here
   $http({
   	method:'POST',
   	url:'/cats',
-  	data:{catName : $scope.catName,
+  	data:{
+  		catName : $scope.catName,
   		ownerEmail: $scope.ownerEmail,
   		imageUrl  : $scope.imageUrl,
   		adoptionMessage: $scope.adoptionMessage
-  	}.then(function (){
+  		}
+  	}).then(function (){
   		console.log('done')
   	}, function () {
   		console.log('error')
   	})
-  })
 }
 
 });
