41fc66b43602ae9f0ed48600a069314754708039
QaysTrad <qaystrad@gamil.com>
2018-08-15 10:52:09 +0300

    make a post requset

--- angular-client/components/catsList.js
+++ angular-client/components/catsList.js
@@ -12,8 +12,19 @@ getCats();
 
 $scope.addCats=function() {
 // TODO: Your code here
-
-
+  $http({
+  	method:'POST',
+  	url:'/cats',
+  	data:{catName : $scope.catName,
+  		ownerEmail: $scope.ownerEmail,
+  		imageUrl  : $scope.imageUrl,
+  		adoptionMessage: $scope.adoptionMessage
+  	}.then(function (){
+  		console.log('done')
+  	}, function () {
+  		console.log('error')
+  	})
+  })
 }
 
 });
